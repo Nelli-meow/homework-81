06d65386925b2f1cc2f9460d79a8ee434bedcaa7
nelli gorlova <nelli7035@gmail.com>
2024-12-26 07:01:24 +0600

    EX81 is done

--- links-frontend/src/types.d.ts
+++ links-frontend/src/types.d.ts
@@ -1,4 +1,4 @@
 export interface ILinkSWithoutID {
-    shortUrl: string;
-    originalUrl: string;
+  shortUrl: string;
+  originalUrl: string;
 }
--- links-frontend/types.d.ts
+++
@@ -1,3 +0,0 @@
-export interface LinksProps {
-  link: string;
-}
